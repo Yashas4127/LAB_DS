d736fe4460fb3ce26c3f2415fcc0b4a8fc7c5c93
YASHAS  H M <yashashm19@gmail.com>
2025-11-23 22:54:29 +0530

    Refactor postfix and prefix evaluation logic
    Refactor evaluation functions for postfix and prefix expressions. Added error handling for division by zero and improved operator evaluation.

--- 03C_Evaluation.c
+++ 03C_Evaluation.c
@@ -1,140 +1,213 @@
 #include <stdio.h>
-#include <ctype.h>
-
-#define MAX 50
-
-typedef struct {
-    float items[MAX];
-    int top;
-} STACK;
-
-void PUSH(STACK *s, float val) {
-    s->items[++s->top] = val;
-}
-
-float POP(STACK *s) {
-    return s->items[s->top--];
-}
-
-// Compute function
-float compute(float n1, char op, float n2) {
-    switch (op) {
-        case '+': return n1 + n2;
-        case '-': return n1 - n2;
-        case '*': return n1 * n2;
-        case '/': return n1 / n2;
-        case '^': {
-            float pow = 1;
-            for (int i = 0; i < (int)n2; i++)
-                pow *= n1;
-            return pow;
-        }
-    }
-    return 0;
-}
-
-int main() {
-    STACK s;
-    s.top = -1;
-
-    char postfix[30], symb;
-    float n1, n2, val, data;
-
-    printf("Enter a valid postfix expression:\n");
-    scanf("%s", postfix);
-
-    for (int i = 0; postfix[i] != '\0'; i++) {
-        symb = postfix[i];
-
-        if (isdigit(symb)) {
-            PUSH(&s, symb - '0');
-        }
-        else if (isalpha(symb)) {
-            printf("Enter value for %c: ", symb);
-            scanf("%f", &data);
-            PUSH(&s, data);
-        }
-        else {
-            n2 = POP(&s);
-            n1 = POP(&s);
-            val = compute(n1, symb, n2);
-            PUSH(&s, val);
-        }
-    }
-
-    printf("Result of evaluation: %f\n", POP(&s));
-    return 0;
-}
-
-
-
-#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
 #include <string.h>
-#include <ctype.h>
 
-#define MAX 50
+// -----------------------------------
+// Evaluate operator
+// -----------------------------------
+float eval(float op1, float op2, char op)
+{
+    switch (op)
+    {
+        case '+': return op1 + op2;
+        case '-': return op1 - op2;
+        case '*': return op1 * op2;
+        case '/':
+            if (op2 == 0)
+            {
+                printf("Division by 0 error!\n");
+                exit(1);
+            }
+            return op1 / op2;
 
-typedef struct {
-    float items[MAX];
-    int top;
-} STACK;
+        case '%':
+            if (op2 == 0)
+            {
+                printf("Division by 0 error!\n");
+                exit(1);
+            }
+            return fmod(op1, op2);
 
-void PUSH(STACK *s, float v) {
-    s->items[++s->top] = v;
-}
-
-float POP(STACK *s) {
-    return s->items[s->top--];
-}
-
-// Compute
-float compute(float n1, char op, float n2) {
-    switch (op) {
-        case '+': return n1 + n2;
-        case '-': return n1 - n2;
-        case '*': return n1 * n2;
-        case '/': return n1 / n2;
-        case '^': {
-            float p = 1;
-            for (int i = 0; i < (int)n2; i++)
-                p *= n1;
-            return p;
-        }
+        case '^': return pow(op1, op2);
     }
-    return 0;
+    printf("Invalid operator!\n");
+    exit(1);
 }
 
-int main() {
-    STACK s;
-    s.top = -1;
+// -----------------------------------
+// POSTFIX Evaluation
+// -----------------------------------
+float evaluatePostfix(char postfix[])
+{
+    float stack[100], smalTab[26], bigTab[26];
+    int top = -1, i = 0, j;
+    char sym;
 
-    char prefix[30], symb;
-    float n1, n2, val, data;
+    for (int k = 0; k < 26; k++)
+    {
+        smalTab[k] = -999;
+        bigTab[k] = -999;
+    }
 
-    printf("Enter a valid prefix expression:\n");
-    scanf("%s", prefix);
+    while ((sym = postfix[i]) != '\0')
+    {
+        if (sym==' ' || sym=='\n' || sym=='\t' || sym=='\r')
+        {
+            i++;
+            continue;
+        }
+
+        if (sym >= '0' && sym <= '9')
+        {
+            stack[++top] = sym - '0';
+        }
+        else if (sym >= 'a' && sym <= 'z')
+        {
+            j = sym - 'a';
+            if (smalTab[j] == -999)
+            {
+                printf("Enter value of %c: ", sym);
+                scanf("%f", &smalTab[j]);
+            }
+            stack[++top] = smalTab[j];
+        }
+        else if (sym >= 'A' && sym <= 'Z')
+        {
+            j = sym - 'A';
+            if (bigTab[j] == -999)
+            {
+                printf("Enter value of %c: ", sym);
+                scanf("%f", &bigTab[j]);
+            }
+            stack[++top] = bigTab[j];
+        }
+        else
+        {
+            if (top < 1)
+            {
+                printf("Invalid postfix expression!\n");
+                exit(1);
+            }
+            float op2 = stack[top--];
+            float op1 = stack[top--];
+            float val = eval(op1, op2, sym);
+            stack[++top] = val;
+        }
+
+        i++;
+    }
+
+    if (top == 0) return stack[top];
+
+    printf("Invalid postfix expression!\n");
+    exit(1);
+}
+
+// -----------------------------------
+// PREFIX Evaluation
+// -----------------------------------
+float evaluatePrefix(char prefix[])
+{
+    float stack[100], smalTab[26], bigTab[26];
+    int top = -1, j;
+    char sym;
+
+    for (int k = 0; k < 26; k++)
+    {
+        smalTab[k] = -999;
+        bigTab[k] = -999;
+    }
 
     int len = strlen(prefix);
 
-    for (int i = len - 1; i >= 0; i--) {
-        symb = prefix[i];
+    for (int i = len - 1; i >= 0; i--)
+    {
+        sym = prefix[i];
 
-        if (isdigit(symb)) {
-            PUSH(&s, symb - '0');
+        if (sym==' ' || sym=='\n' || sym=='\t' || sym=='\r')
+            continue;
+
+        if (sym >= '0' && sym <= '9')
+        {
+            stack[++top] = sym - '0';
         }
-        else if (isalpha(symb)) {
-            printf("Enter value for %c: ", symb);
-            scanf("%f", &data);
-            PUSH(&s, data);
+        else if (sym >= 'a' && sym <= 'z')
+        {
+            j = sym - 'a';
+            if (smalTab[j] == -999)
+            {
+                printf("Enter value of %c: ", sym);
+                scanf("%f", &smalTab[j]);
+            }
+            stack[++top] = smalTab[j];
         }
-        else {  // operator
-            n1 = POP(&s);
-            n2 = POP(&s);
-            val = compute(n1, symb, n2);
-            PUSH(&s, val);
+        else if (sym >= 'A' && sym <= 'Z')
+        {
+            j = sym - 'A';
+            if (bigTab[j] == -999)
+            {
+                printf("Enter value of %c: ", sym);
+                scanf("%f", &bigTab[j]);
+            }
+            stack[++top] = bigTab[j];
+        }
+        else
+        {
+            if (top < 1)
+            {
+                printf("Invalid prefix expression!\n");
+                exit(1);
+            }
+
+            float op1 = stack[top--];
+            float op2 = stack[top--];
+
+            float val = eval(op1, op2, sym);
+            stack[++top] = val;
         }
     }
 
-    printf("Result of evaluation: %f\n", POP(&s));
+    if (top == 0) return stack[top];
+
+    printf("Invalid prefix expression!\n");
+    exit(1);
+}
+
+// -----------------------------------
+// MAIN PROGRAM – Both Evaluators
+// -----------------------------------
+int main()
+{
+    char expr[100];
+    int choice;
+    float result;
+
+    printf("\n--- Combined Prefix & Postfix Evaluation Program ---\n");
+    printf("1. Evaluate Postfix\n");
+    printf("2. Evaluate Prefix\n");
+    printf("Enter choice: ");
+    scanf("%d", &choice);
+
+    getchar();  // remove leftover newline
+
+    printf("Enter expression:\n");
+    fgets(expr, 100, stdin);
+
+    if (choice == 1)
+    {
+        result = evaluatePostfix(expr);
+        printf("Postfix Result = %.2f\n", result);
+    }
+    else if (choice == 2)
+    {
+        result = evaluatePrefix(expr);
+        printf("Prefix Result = %.2f\n", result);
+    }
+    else
+    {
+        printf("Invalid choice!\n");
+    }
+
     return 0;
 }
